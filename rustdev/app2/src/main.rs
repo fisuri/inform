37d5a857413c9d22c23d6f00a67b97f40a12f293
Yaroslav Malykh <yaroslav.malykh@gmail.com>
2023-05-08 16:25:24 +0400

    refactor(main.rs): move parse_to_f64 function inside Triangle struct to improve encapsulation and readability feat(main.rs): add match statement to output whether tr2 fits inside tr1 or not

--- rustdev/app2/src/main.rs
+++ rustdev/app2/src/main.rs
@@ -1,22 +1,29 @@
 use std::io;
 
 fn main() {
-    let cat1: f64 = parse_to_f64("Введите первый катет");
-    let cat2: f64 = parse_to_f64("Введите второй катет");
+    // Стринговые переменные для записи в них значений пользователя
+    let cat1: f64 = Triangle::parse_to_f64("Введите первый катет");
+    let cat2: f64 = Triangle::parse_to_f64("Введите второй катет");
 
-    let cat1_tr2: f64 = parse_to_f64("Введите первый катет второго треугольника");
-    let cat2_tr2: f64 = parse_to_f64("Введите второй катет второго треугольника");
+    let cat1_tr2: f64 = Triangle::parse_to_f64("Введите первый катет второго треугольника");
+    let cat2_tr2: f64 = Triangle::parse_to_f64("Введите второй катет второго треугольника");
 
+    // Обращение к структуре Triangle для tr1 и tr2
     let tr1 = Triangle { cat1, cat2 };
     let tr2 = Triangle {
         cat1: cat1_tr2,
         cat2: cat2_tr2,
     };
 
+    // Нахождение гипотенузы и площади tr1
     let hyp = tr1.find_hyp();
     let area = tr1.find_area();
 
-    let result = tr2.is_eq(area);
+    // Выводим результат того, влезает ли tr2 в tr1
+    let result = match tr2.is_eq(area) {
+        true => "Да".to_string(),
+        false => "Нет".to_string(),
+    };
 
     println!(
         "Гипотинуза - {}, площадь - {}, помещается ли tr2 в tr1 - {}",
@@ -30,30 +37,34 @@ struct Triangle {
 }
 
 impl Triangle {
+    // Нахождение гипотенузы
     fn find_hyp(&self) -> f64 {
         (self.cat1 * self.cat1 + self.cat2 * self.cat2).sqrt()
     }
 
+    // Нахождение площади
     fn find_area(&self) -> f64 {
         0.5 * self.cat1 * self.cat2
     }
 
+    // Влезет ли tr2 в tr1
     fn is_eq(&self, ar: f64) -> bool {
         self.find_area() < ar
     }
-}
 
-fn parse_to_f64(prompt: &str) -> f64 {
-    println!("{prompt}");
+    // Конвертируем стринговые переменные в f64
+    fn parse_to_f64(prompt: &str) -> f64 {
+        println!("{prompt}");
 
-    let mut value = String::new();
+        let mut value = String::new();
 
-    io::stdin()
-        .read_line(&mut value)
-        .expect("Не удалось прочитать строку");
+        io::stdin()
+            .read_line(&mut value)
+            .expect("Не удалось прочитать строку");
 
-    value
-        .trim()
-        .parse::<f64>()
-        .expect("Failed to parse input as f64")
+        value
+            .trim()
+            .parse::<f64>()
+            .expect("Failed to parse input as f64")
+    }
 }
